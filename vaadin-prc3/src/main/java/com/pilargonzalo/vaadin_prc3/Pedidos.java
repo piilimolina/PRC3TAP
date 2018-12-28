a82246e86fda6c242d115f86973c0279cc86e04e
Pilar Molina <cordoba.pmolina@hotmail.com>
2018-12-28 19:50:37 +0100

    Update Pedidos.java

--- vaadin-prc3/src/main/java/com/pilargonzalo/vaadin_prc3/Pedidos.java
+++ vaadin-prc3/src/main/java/com/pilargonzalo/vaadin_prc3/Pedidos.java
@@ -18,20 +18,7 @@ public class Pedidos {
 	//Eliminar un producto del pedido
 	public void deleteProdToP(Producto p) {
 		productos.remove(p);
-	}
-	
-	public void modificarProdToPed(Producto p, Producto n){
-		Iterator<Producto> it = productos.iterator();
-		while (it.hasNext()) {
-			if((p).equals(it.next())) {
-				System.out.println(p+"jlkjdskl"+ it.next());
-				productos.remove(p);
-				productos.add(n);
-			}
-		}
-
-	}
-	
+	}	
 	//Mostrar resumen del pedido
 	public List<Producto> getProductos(){
 		return productos;
