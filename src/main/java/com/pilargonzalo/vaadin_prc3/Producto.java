c7e17906c6c6de957f314d87f18847d3ae2c032e
Gonxho <zalofdezgtk@gmail.com>
2019-01-07 17:33:49 +0100

    Heroku

---
+++ src/main/java/com/pilargonzalo/vaadin_prc3/Producto.java
@@ -0,0 +1,56 @@
+package com.pilargonzalo.vaadin_prc3;
+
+import java.util.ArrayList;
+
+public class Producto {
+	
+	public  String nombre;
+	public  Double precio;
+	public  int cantidad;
+	private ArrayList<Componente> componente = new ArrayList<>();
+	
+	public Producto(String nombre, Double precio, int cantidad, ArrayList<Componente> componente) {
+		super(); 
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidad = cantidad;
+		this.componente = componente;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public ArrayList<Componente> getComponente() {
+		return componente;
+	}
+
+	public void setComponente(ArrayList<Componente> componente) {
+		this.componente = componente;
+	}
+
+
+
+
+	
+}
